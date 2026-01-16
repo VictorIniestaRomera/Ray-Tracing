a09db45190766c65a1ec57969044ca4f1e4722cc
VictorIniestaRomera <victorinirom@campus.monlau.com>
2026-01-16 12:21:19 +0100

    Version 2
    Add a red sphere on the middle

--- RayTracing.cpp
+++ RayTracing.cpp
@@ -1,6 +1,19 @@
 ﻿#include "RayTracing.h"
 
+bool hit_sphere(const Point3& center, double radius, const Ray& r) {
+	Vector3 oc = center - r.origin();
+
+	double a = dot(r.direction(), r.direction());
+	double b = -2.0 * dot(r.direction(), oc);
+	double c = dot(oc, oc) - radius * radius;
+	double discriminant = b * b - 4 * a * c;
+
+	return (discriminant >= 0);
+}
+
 Color ray_color(const Ray& r) {
+	if (hit_sphere(Point3(0, 0, -1), 0.5, r)) return Color(1, 0, 0);
+
 	Vector3 unitDirection = unit_vector(r.direction());
 	double a = 0.5 * (unitDirection.y() + 1.0);
 
